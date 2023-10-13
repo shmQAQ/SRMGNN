4a7dcc748d033f478ba66723d1e5b50840f7542c
shmQAQ <1957721668@qq.com>
2023-10-13 20:07:05 +0800

    update

--- data.py
+++ data.py
@@ -1,1 +1,143 @@
-from
+import os
+import pandas as pd
+import torch
+import torch_geometric.utils as utils
+from dgllife.utils import CanonicalAtomFeaturizer, CanonicalBondFeaturizer, smiles_to_bigraph
+
+class MoleculeDataset(object):
+    '''
+    define the dataset for molecule
+    input: 
+        file_path: the path of the dataset
+        task_name: the name of the task
+        node_featurizer: the featurizer for node: dgllife.utils.CanonicalAtomFeaturizer
+        edge_featurizer: the featurizer for edge: dgllife.utils.CanonicalBondFeaturizer
+        k: the hop number for the k_hop graph
+           
+    process: 
+        read the dataset from the file_path
+        extract smiles and labels
+        generate the graph data
+        generate the k_hop graph data
+    output: 
+        the graph data
+    '''
+
+    def __init__(self, file_path: str, task_name: str, node_featurizer=CanonicalAtomFeaturizer, 
+                 edge_featurizer=CanonicalBondFeaturizer, k: int = 3, cache_path=None, use_subgraph_edge_attr=True):
+        self.file_path = file_path
+        self.task_name = task_name
+        self.cache_path = cache_path
+        self.pd_data = pd.read_csv(self.file_path)
+        self.smiles = self.pd_data['smiles']
+        self.labels = self.pd_data[self.task_name]
+        self.node_featurizer = node_featurizer()
+        self.edge_featurizer = edge_featurizer()
+        self.k = k
+        self.graphs = self.generate_graphs()
+        self.k_hop_graphs = self.generate_k_hop_graphs(cache_path, use_subgraph_edge_attr)
+        self.degree = self.get_degree()
+
+    def __len__(self):
+        return len(self.smiles)
+    
+    def __inc__(self, key, value):
+        if key == 'subgraph_edge_index':
+            return self.num_subgraph_nodes 
+        if key == 'subgraph_node_idx':
+            return self.num_nodes 
+        if key == 'subgraph_indicator':
+            return self.num_nodes 
+        elif 'index' in key:
+            return self.num_nodes
+        else:
+            return 0
+    
+    def __getitem__(self, idx):
+        return self.graphs[idx]
+
+    def generate_graphs(self):
+        graphs = []
+        for i in range(len(self.smiles)):
+            g = smiles_to_bigraph(self.smiles[i], node_featurizer=self.node_featurizer, 
+                                  edge_featurizer=self.edge_featurizer)
+            g = self.add_label(g, self.labels[i])
+            g = g.from_dgl(g)
+            graphs.append(g)
+        return graphs
+    
+    def generate_k_hop_graphs(self, cache_path=None, use_subgraph_edge_attr=True):
+        print("Extracting {}-hop subgraphs...".format(self.k))
+        # indicate which node in a graph it is; for each graph, the
+        # indices will range from (0, num_nodes). PyTorch will then
+        # increment this according to the batch size
+        self.subgraph_node_index = []
+
+        # Each graph will become a block diagonal adjacency matrix of
+        # all the k-hop subgraphs centered around each node. The edge
+        # indices get augumented within a given graph to make this
+        # happen (and later are augmented for proper batching)
+        self.subgraph_edge_index = []
+
+        # This identifies which indices correspond to which subgraph
+        # (i.e. which node in a graph)
+        self.subgraph_indicator_index = []
+
+        # This gets the edge attributes for the new indices
+        if use_subgraph_edge_attr:
+            self.subgraph_edge_attr = []
+
+        for i in range(len(self)):
+            if cache_path is not None:
+                filepath = "{}_{}.pt".format(cache_path, i)
+                if os.path.exists(filepath):
+                    continue
+            graph = self[i]
+            node_indices = []
+            edge_indices = []
+            edge_attributes = []
+            indicators = []
+            edge_index_start = 0
+
+            for node_idx in range(graph.num_nodes):
+                sub_nodes, sub_edge_index, _, edge_mask = utils.k_hop_subgraph(
+                    node_idx, 
+                    self.k, 
+                    graph.edge_index,
+                    relabel_nodes=True, 
+                    num_nodes=graph.num_nodes
+                    )
+                node_indices.append(sub_nodes)
+                edge_indices.append(sub_edge_index + edge_index_start)
+                indicators.append(torch.zeros(sub_nodes.shape[0]).fill_(node_idx))
+                if use_subgraph_edge_attr and graph.edge_attr is not None:
+                    edge_attributes.append(graph.edge_attr[edge_mask]) # CHECK THIS DIDN"T BREAK ANYTHING
+                edge_index_start += len(sub_nodes)
+
+            if cache_path is not None:
+                if use_subgraph_edge_attr and graph.edge_attr is not None:
+                    subgraph_edge_attr = torch.cat(edge_attributes)
+                else:
+                    subgraph_edge_attr = None
+                torch.save({
+                    'subgraph_node_index': torch.cat(node_indices),
+                    'subgraph_edge_index': torch.cat(edge_indices, dim=1),
+                    'subgraph_indicator_index': torch.cat(indicators).type(torch.LongTensor),
+                    'subgraph_edge_attr': subgraph_edge_attr
+                }, filepath)
+            else:
+                self.subgraph_node_index.append(torch.cat(node_indices))
+                self.subgraph_edge_index.append(torch.cat(edge_indices, dim=1))
+                self.subgraph_indicator_index.append(torch.cat(indicators))
+                if self.use_subgraph_edge_attr and graph.edge_attr is not None:
+                    self.subgraph_edge_attr.append(torch.cat(edge_attributes))
+        print("Done!")
+
+    def get_degree(self):
+        if not self.degree:
+            self.degree_list = None
+            return
+        self.degree_list = []
+        for g in self.graphs:
+            deg = 1. / torch.sqrt(1. + utils.degree(g.edge_index[0], g.num_nodes))
+            self.degree_list.append(deg)
---
+++ model.py
@@ -0,0 +1,198 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric.nn as pyg_nn
+from torch_geometric import utils
+from torch_geometric.nn.aggr import Aggregation
+from torch_scatter import scatter
+from torch.nn import ModuleList, Sequential, Linear, ReLU, BatchNorm1d, Dropout
+from torch import Tensor
+from torch_geometric.typing import Adj, OptTensor
+from typing import Any, Optional, List, Dict, Union
+from torch_geometric.nn.inits import reset
+from torch_geometric.utils import degree
+
+class SqaredReLu(nn.Module):
+    '''fast implementation of squared relu function'''
+    def __init__(self):
+        super(SqaredReLu, self).__init__()
+    
+    def forward(self, x):
+        return torch.pow(F.relu(x), 2)
+    
+
+class SpatialDepthWiseConv(nn.Module):
+    '''
+    the spatial depthwise convolution in front of q,k,v
+    '''
+    def __init__(self,d_k:int, kenerl_size:int=3):
+        super(SpatialDepthWiseConv, self).__init__()
+        self.conv = nn.Conv1d(d_k, d_k, kenerl_size, padding=kenerl_size-1, groups=d_k)
+        
+    def forward(self,x):
+        seq_len,batch_size,heads,d_k = x.shape
+        x = x.permute(1,2,3,0)
+        x = x.view(batch_size*heads,d_k,seq_len)
+        x = self.conv(x)
+        x = x[:, :, :-(self.kernel_size-1)]
+        x = x.view(batch_size,heads,d_k,seq_len)
+        x = x.permute(3,0,1,2)
+        return x
+
+class Attention(pyg_nn.MessagePassing):
+    '''
+    multihead structure attention
+    input:batch of graph data from pyg
+    arg:
+    embed_dim:int the dimension of the embedding
+    num_heads:int the number of the heads
+    dropout:float the dropout rate
+    bias:bool whether to use bias
+    symmetric:bool whether to use symmetric attention
+    k:number of k-hop neighbors
+    '''
+    def __init__(self, embed_dim, num_heads=8, dropout=0., bias=False,
+        symmetric=False, k_hop=3, **kwargs):
+
+        super().__init__(node_dim=0, aggr='add')
+        self.embed_dim = embed_dim
+        self.bias = bias
+        head_dim = embed_dim // num_heads
+        assert head_dim * num_heads == embed_dim, "embed_dim must be divisible by num_heads"
+
+        self.num_heads = num_heads
+        self.scale = head_dim ** -0.5
+        self.khop_structure_extractor = KHopStructureExtractor(embed_dim, num_layers=k_hop, **kwargs)
+        self.attend = nn.Softmax(dim=-1)
+
+        self.symmetric = symmetric
+        if symmetric:
+            self.to_qk = nn.Linear(embed_dim, embed_dim, bias=bias)
+        else:
+            self.to_qk = nn.Linear(embed_dim, embed_dim * 2, bias=bias)
+        self.to_v = nn.Linear(embed_dim, embed_dim, bias=bias)
+
+        self.attn_dropout = nn.Dropout(dropout)
+
+        self.out_proj = nn.Linear(embed_dim, embed_dim)
+
+        self._reset_parameters()
+
+        self.attn_sum = None
+
+    def _reset_parameters(self):
+        nn.init.xavier_uniform_(self.to_qk.weight)
+        nn.init.xavier_uniform_(self.to_v.weight)
+
+        if self.bias:
+            nn.init.constant_(self.to_qk.bias, 0.)
+            nn.init.constant_(self.to_v.bias, 0.)
+
+    def forward(self,
+            x,
+            edge_index,
+            complete_edge_index,
+            subgraph_node_index=None,
+            subgraph_edge_index=None,
+            subgraph_indicator_index=None,
+            subgraph_edge_attr=None,
+            edge_attr=None,
+            ptr=None,
+            return_attn=False):
+        """
+        Compute attention layer. 
+
+        Args:
+        ----------
+        x:                          input node features
+        edge_index:                 edge index from the graph
+        complete_edge_index:        edge index from fully connected graph
+        subgraph_node_index:        documents the node index in the k-hop subgraphs
+        subgraph_edge_index:        edge index of the extracted subgraphs 
+        subgraph_indicator_index:   indices to indicate to which subgraph corresponds to which node
+        subgraph_edge_attr:         edge attributes of the extracted k-hop subgraphs
+        edge_attr:                  edge attributes
+        return_attn:                return attention (default: False)
+
+        """
+        # Compute value matrix
+
+        v = self.to_v(x)
+
+        # Compute structure-aware node embeddings 
+        x_struct = self.khop_structure_extractor(
+                x=x,
+                edge_index=edge_index,
+                subgraph_edge_index=subgraph_edge_index,
+                subgraph_indicator_index=subgraph_indicator_index,
+                subgraph_node_index=subgraph_node_index,
+                subgraph_edge_attr=subgraph_edge_attr,
+            )
+
+
+        # Compute query and key matrices
+        if self.symmetric:
+            qk = self.to_qk(x_struct)
+            qk = (qk, qk)
+        else:
+            qk = self.to_qk(x_struct).chunk(2, dim=-1)
+        
+        # Compute complete self-attention
+        attn = None
+
+        if complete_edge_index is not None:
+            out = self.propagate(complete_edge_index, v=v, qk=qk, edge_attr=None, size=None,
+                                 return_attn=return_attn)
+            if return_attn:
+                attn = self._attn
+                self._attn = None
+                attn = torch.sparse_coo_tensor(
+                    complete_edge_index,
+                    attn,
+                ).to_dense().transpose(0, 1)
+
+            out = rearrange(out, 'n h d -> n (h d)')
+        else:
+            out, attn = self.self_attn(qk, v, ptr, return_attn=return_attn)
+        return self.out_proj(out), attn
+
+    def message(self, v_j, qk_j, qk_i, edge_attr, index, ptr, size_i, return_attn):
+        """Self-attention operation compute the dot-product attention """
+
+        qk_i = rearrange(qk_i, 'n (h d) -> n h d', h=self.num_heads)
+        qk_j = rearrange(qk_j, 'n (h d) -> n h d', h=self.num_heads)
+        v_j = rearrange(v_j, 'n (h d) -> n h d', h=self.num_heads)
+        attn = (qk_i * qk_j).sum(-1) * self.scale
+        if edge_attr is not None:
+            attn = attn + edge_attr
+        attn = utils.softmax(attn, index, ptr, size_i)
+        if return_attn:
+            self._attn = attn
+        attn = self.attn_dropout(attn)
+
+        return v_j * attn.unsqueeze(-1)
+
+    def self_attn(self, qk, v, ptr, return_attn=False):
+        """ Self attention which can return the attn """ 
+
+        qk, mask = pad_batch(qk, ptr, return_mask=True)
+        k, q = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.num_heads), qk)
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        dots = dots.masked_fill(
+            mask.unsqueeze(1).unsqueeze(2),
+            float('-inf'),
+        )
+
+        dots = self.attend(dots)
+        dots = self.attn_dropout(dots)
+
+        v = pad_batch(v, ptr)
+        v = rearrange(v, 'b n (h d) -> b h n d', h=self.num_heads)
+        out = torch.matmul(dots, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out = unpad_batch(out, ptr)
+
+        if return_attn:
+            return out, dots
+        return out, None
