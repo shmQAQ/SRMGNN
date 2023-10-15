f33be952fb1cccee5e8b8d356feb25164d009160
shmQAQ <1957721668@qq.com>
2023-10-15 20:13:34 +0800

    Update model.py

--- model.py
+++ model.py
@@ -4,14 +4,14 @@ import torch.nn.functional as F
 import torch_geometric.nn as pyg_nn
 from torch_geometric import utils
 from torch_geometric.nn.aggr import Aggregation
-from torch_scatter import scatter
+from torch_scatter import  scatter_add, scatter_mean
 from torch.nn import ModuleList, Sequential, Linear, ReLU, BatchNorm1d, Dropout
 from torch import Tensor
 from torch_geometric.typing import Adj, OptTensor
 from typing import Any, Optional, List, Dict, Union
 from torch_geometric.nn.inits import reset
 from torch_geometric.utils import degree
-
+from einops import rearrange
 
 class SqaredReLu(nn.Module):
     '''fast implementation of squared relu function'''
@@ -203,3 +203,195 @@ class Attention(pyg_nn.MessagePassing):
         if return_attn:
             return out, dots
         return out, None
+
+class StructureExtractor(nn.Module):
+    r""" K-subtree structure extractor. Computes the structure-aware node embeddings using the
+    k-hop subtree centered around each node.
+
+    Args:
+    ----------
+    embed_dim (int):        the embeding dimension
+    gnn_type (str):         GNN type to use in structure extractor. (gcn, gin, pna, etc)
+    num_layers (int):       number of GNN layers
+    batch_norm (bool):      apply batch normalization or not
+    concat (bool):          whether to concatenate the initial edge features
+    khopgnn (bool):         whether to use the subgraph instead of subtree
+    """
+
+    def __init__(self, embed_dim, gnn_type="gcn", num_layers=3,
+                 batch_norm=True, concat=True, khopgnn=False, **kwargs):
+        super().__init__()
+        self.num_layers = num_layers
+        self.khopgnn = khopgnn
+        self.concat = concat
+        self.gnn_type = gnn_type
+        layers = []
+        for _ in range(num_layers):
+            layers.append(get_simple_gnn_layer(gnn_type, embed_dim, **kwargs))
+        self.gcn = nn.ModuleList(layers)
+
+        self.relu = nn.ReLU()
+        self.batch_norm = batch_norm
+        inner_dim = (num_layers + 1) * embed_dim if concat else embed_dim
+
+        if batch_norm:
+            self.bn = nn.BatchNorm1d(inner_dim)
+
+        self.out_proj = nn.Linear(inner_dim, embed_dim)
+
+    def forward(self, x, edge_index, edge_attr=None,
+            subgraph_indicator_index=None, agg="sum"):
+        x_cat = [x]
+        for gcn_layer in self.gcn:
+            # if self.gnn_type == "attn":
+            #     x = gcn_layer(x, edge_index, None, edge_attr=edge_attr)
+            if self.gnn_type in EDGE_GNN_TYPES:
+                if edge_attr is None:
+                    x = self.relu(gcn_layer(x, edge_index))
+                else:
+                    x = self.relu(gcn_layer(x, edge_index, edge_attr=edge_attr))
+            else:
+                x = self.relu(gcn_layer(x, edge_index))
+
+            if self.concat:
+                x_cat.append(x)
+
+        if self.concat:
+            x = torch.cat(x_cat, dim=-1)
+
+        if self.khopgnn:
+            if agg == "sum":
+                x = scatter_add(x, subgraph_indicator_index, dim=0)
+            elif agg == "mean":
+                x = scatter_mean(x, subgraph_indicator_index, dim=0)
+            return x
+
+        if self.num_layers > 0 and self.batch_norm:
+            x = self.bn(x)
+
+        x = self.out_proj(x)
+        return x
+
+
+class KHopStructureExtractor(nn.Module):
+    r""" K-subgraph structure extractor. Extracts a k-hop subgraph centered around
+    each node and uses a GNN on each subgraph to compute updated structure-aware
+    embeddings.
+
+    Args:
+    ----------
+    embed_dim (int):        the embeding dimension
+    gnn_type (str):         GNN type to use in structure extractor. (gcn, gin, pna, etc)
+    num_layers (int):       number of GNN layers
+    concat (bool):          whether to concatenate the initial edge features
+    khopgnn (bool):         whether to use the subgraph instead of subtree (True)
+    """
+    def __init__(self, embed_dim, gnn_type="gcn", num_layers=3, batch_norm=True,
+            concat=True, khopgnn=True, **kwargs):
+        super().__init__()
+        self.num_layers = num_layers
+        self.khopgnn = khopgnn
+
+        self.batch_norm = batch_norm
+
+        self.structure_extractor = StructureExtractor(
+            embed_dim,
+            gnn_type=gnn_type,
+            num_layers=num_layers,
+            concat=False,
+            khopgnn=True,
+            **kwargs
+        )
+
+        if batch_norm:
+            self.bn = nn.BatchNorm1d(2 * embed_dim)
+
+        self.out_proj = nn.Linear(2 * embed_dim, embed_dim)
+
+    def forward(self, x, edge_index, subgraph_edge_index, edge_attr=None,
+            subgraph_indicator_index=None, subgraph_node_index=None,
+            subgraph_edge_attr=None):
+
+        x_struct = self.structure_extractor(
+            x=x[subgraph_node_index],
+            edge_index=subgraph_edge_index,
+            edge_attr=subgraph_edge_attr,
+            subgraph_indicator_index=subgraph_indicator_index,
+            agg="sum",
+        )
+        x_struct = torch.cat([x, x_struct], dim=-1)
+        if self.batch_norm:
+            x_struct = self.bn(x_struct)
+        x_struct = self.out_proj(x_struct)
+
+        return x_struct
+
+
+class TransformerEncoderLayer(nn.TransformerEncoderLayer):
+    r"""Structure-Aware Transformer layer, made up of structure-aware self-attention and feed-forward network.
+
+    Args:
+    ----------
+        d_model (int):      the number of expected features in the input (required).
+        nhead (int):        the number of heads in the multiheadattention models (default=8).
+        dim_feedforward (int): the dimension of the feedforward network model (default=512).
+        dropout:            the dropout value (default=0.1).
+        activation:         the activation function of the intermediate layer, can be a string
+            ("relu" or "gelu") or a unary callable (default: relu).
+        batch_norm:         use batch normalization instead of layer normalization (default: True).
+        pre_norm:           pre-normalization or post-normalization (default=False).
+        gnn_type:           base GNN model to extract subgraph representations.
+                            One can implememnt customized GNN in gnn_layers.py (default: gcn).
+        se:                 structure extractor to use, either gnn or khopgnn (default: gnn).
+        k_hop:              the number of base GNN layers or the K hop size for khopgnn structure extractor (default=2).
+    """
+    def __init__(self, d_model, nhead=8, dim_feedforward=512, dropout=0.1,
+                activation="relu", batch_norm=True, pre_norm=False,
+                 k_hop=2, **kwargs):
+        super().__init__(d_model, nhead, dim_feedforward, dropout, activation)
+
+        self.self_attn = Attention(d_model, nhead, dropout=dropout,
+            bias=False, k_hop=k_hop, **kwargs)
+        self.batch_norm = batch_norm
+        self.pre_norm = pre_norm
+        if batch_norm:
+            self.norm1 = nn.BatchNorm1d(d_model)
+            self.norm2 = nn.BatchNorm1d(d_model)
+
+    def forward(self, x, edge_index, complete_edge_index,
+            subgraph_node_index=None, subgraph_edge_index=None,
+            subgraph_edge_attr=None,
+            subgraph_indicator_index=None,
+            edge_attr=None, degree=None, ptr=None,
+            return_attn=False,
+        ):
+
+        if self.pre_norm:
+            x = self.norm1(x)
+
+        x2, attn = self.self_attn(
+            x,
+            edge_index,
+            complete_edge_index,
+            edge_attr=edge_attr,
+            subgraph_node_index=subgraph_node_index,
+            subgraph_edge_index=subgraph_edge_index,
+            subgraph_indicator_index=subgraph_indicator_index,
+            subgraph_edge_attr=subgraph_edge_attr,
+            ptr=ptr,
+            return_attn=return_attn
+        )
+
+        if degree is not None:
+            x2 = degree.unsqueeze(-1) * x2
+        x = x + self.dropout1(x2)
+        if self.pre_norm:
+            x = self.norm2(x)
+        else:
+            x = self.norm1(x)
+        x2 = self.linear2(self.dropout(self.activation(self.linear1(x))))
+        x = x + self.dropout2(x2)
+
+        if not self.pre_norm:
+            x = self.norm2(x)
+        return x
